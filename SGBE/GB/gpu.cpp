d301273dd7bac26f57cf63ad2e42b97f2d17157e
Dedi Sidi <dedisidi@gmail.com>
2020-07-29 10:32:56 +0300

    added LCD Transfer mode (mode 3) WITHOUT the actual scanline method

--- SGBE/GB/gpu.cpp
+++ SGBE/GB/gpu.cpp
@@ -2,8 +2,26 @@
 
 GPU::GPU(Gameboy& i_Gameboy) : m_Gameboy(i_Gameboy), m_Mode(Video_Mode::Searching_OAM), m_VideoCycles(0) {}
 
+
+/*
+	Taken from Pan Docs:
+	The following are typical when the display is enabled:
+	Mode 2  2_____2_____2_____2_____2_____2___________________2____
+	Mode 3  _33____33____33____33____33____33__________________3___
+	Mode 0  ___000___000___000___000___000___000________________000
+	Mode 1  ____________________________________11111111111111_____
+
+	the gpu switches between the modes in the way above.
+	modes 2 (Search OAM), 3 (transfer data to LCD), 0 (HBlank) untill 144 of the lines have been drawn (sent to frame buffer)
+	then mode 1 (VBlank) happens for the next 10 lines 144-153
+	all of this happens per frame and in total it takes 70224 cycles
+*/
 void GPU::Step(uint32_t& i_Cycles)
 {
+	// synchronization explained:
+	// in order to synchronize between the CPU and GPU in the emulation,
+	// each step we update the member m_VideoCycles by the cycles that the cpu did in the last step
+	// only once the cpu have made enough (the minimum) cycles that correspond to the current mode, then we actualy do the mode operation
 	if (isLCDEnabled())
 	{
 		m_VideoCycles += i_Cycles;
@@ -21,8 +39,6 @@ void GPU::Step(uint32_t& i_Cycles)
 		case GPU::Video_Mode::Transfer_Data_To_LCD:
 			handleLCDTransferMode();
 			break;
-		default:
-			break;
 		}
 	}
 }
@@ -36,9 +52,10 @@ void GPU::Reset()
 bool GPU::isLCDEnabled()
 {
 	byte LCDC = m_Gameboy.GetMMU().Read(GPU_LCD_CONTROL_ADDR);
-	return bitwise::GetBit(LCD_DISPLAY_ENABLE_BIT, LCDC);
+	return bitwise::GetBit(LCD_CONTROL_LCD_DISPLAY_ENABLE_BIT, LCDC);
 }
 
+/* mode 0 handler */
 void GPU::handleHBlankMode()
 {
 
@@ -49,19 +66,45 @@ void GPU::handleVBlankMode()
 
 }
 
+/* mode 2 handler */
 void GPU::handleSearchSpritesAttributesMode()
 {
 	if (m_VideoCycles >= MIN_SEARCHING_OAM_MODE_CYCLES)
 	{
 		m_VideoCycles %= MIN_SEARCHING_OAM_MODE_CYCLES;
-		// move to mode 3
+
+		// move to mode 3 (Transfer data to LCD)
 		setMode(Video_Mode::Transfer_Data_To_LCD);
 	}
 }
 
+/* mode 3 handler */
 void GPU::handleLCDTransferMode()
 {
+	if (m_VideoCycles >= MIN_TRANSFER_DATA_TO_LCD_MODE_CYCLES)
+	{
+		m_VideoCycles %= MIN_TRANSFER_DATA_TO_LCD_MODE_CYCLES;
 
+		// write a single scanline
+		drawScanline();
+
+		// check for mode 0 (H Blank) interrupt bit
+		if (checkForLCDCInterrupt(LCDC_STATUS_MODE_0_H_BLANK_INTERRUPT_BIT))
+		{
+			m_Gameboy.GetCPU().RequestInterrupt(CPU::InterruptType::LCD);
+		}
+
+		// move to mode 0 (H Blank)
+		setMode(Video_Mode::H_Blank);
+	}
+}
+
+bool GPU::checkForLCDCInterrupt(int i_InterruptBit)
+{
+	bool isInterruptBitRaised = false;
+	byte lcdcStatus = m_Gameboy.GetMMU().Read(GPU_LCDC_STATUS_ADDR);
+	isInterruptBitRaised = bitwise::GetBit(i_InterruptBit, lcdcStatus);
+	return isInterruptBitRaised;
 }
 
 void GPU::setMode(Video_Mode i_NewMode)
@@ -72,26 +115,26 @@ void GPU::setMode(Video_Mode i_NewMode)
 	{
 	case GPU::Video_Mode::H_Blank:
 		// mode 0
-		bitwise::SetBit(0, false, lcdcStatus);
-		bitwise::SetBit(1, false, lcdcStatus);
+		bitwise::SetBit(LCDC_STATUS_MODE_FLAG_FIRST_BIT, false, lcdcStatus);
+		bitwise::SetBit(LCDC_STATUS_MODE_FLAG_SECOND_BIT, false, lcdcStatus);
 		m_Mode = Video_Mode::H_Blank;
 		break;
 	case GPU::Video_Mode::V_Blank:
 		// mode 1
-		bitwise::SetBit(0, true, lcdcStatus);
-		bitwise::SetBit(1, false, lcdcStatus);
+		bitwise::SetBit(LCDC_STATUS_MODE_FLAG_FIRST_BIT, true, lcdcStatus);
+		bitwise::SetBit(LCDC_STATUS_MODE_FLAG_SECOND_BIT, false, lcdcStatus);
 		m_Mode = Video_Mode::V_Blank;
 		break;
 	case GPU::Video_Mode::Searching_OAM:
 		// mode 2
-		bitwise::SetBit(0, false, lcdcStatus);
-		bitwise::SetBit(1, true, lcdcStatus);
+		bitwise::SetBit(LCDC_STATUS_MODE_FLAG_FIRST_BIT, false, lcdcStatus);
+		bitwise::SetBit(LCDC_STATUS_MODE_FLAG_SECOND_BIT, true, lcdcStatus);
 		m_Mode = Video_Mode::Searching_OAM;
 		break;
 	case GPU::Video_Mode::Transfer_Data_To_LCD:
 		// mode 3
-		bitwise::SetBit(0, true, lcdcStatus);
-		bitwise::SetBit(1, true, lcdcStatus);
+		bitwise::SetBit(LCDC_STATUS_MODE_FLAG_FIRST_BIT, true, lcdcStatus);
+		bitwise::SetBit(LCDC_STATUS_MODE_FLAG_SECOND_BIT, true, lcdcStatus);
 		m_Mode = Video_Mode::Transfer_Data_To_LCD;
 		break;
 	}
--- SGBE/GB/gpu.h
+++ SGBE/GB/gpu.h
@@ -19,28 +19,37 @@
  *  http://marc.rawer.de/Gameboy/Docs/GBCPUman.pdf           */
 
  /* gpu (ppu) releated registers addresses in memory */
-const word GPU_LCD_CONTROL_ADDR = 0xFF40; // R/W
-const word GPU_LCDC_STATUS_ADDR = 0xFF41; // R/W
-const word GPU_SCROLL_Y_ADDR = 0xFF42; // R/W
-const word GPU_SCROLL_X_ADDR = 0xFF43; // R/W
-const word GPU_LCDC_Y_CORRDINATE_ADDR = 0xFF44; // Read Only
-const word GPU_LY_COMPARE_ADDR = 0xFF45; // R/W
-const word GPU_DMA_TRANSFER_AND_START_ADDR = 0xFF46; // Write Only
-const word GPU_BG_PALETTE_DATA_ADDR = 0xFF47; // R/W
-const word GPU_OBJECT_PALETTE_0_DATA_ADDR = 0xFF48; // R/W
-const word GPU_OBJECT_PALETTE_1_DATA_ADDR = 0xFF49; // R/W
-const word GPU_WINDOW_Y_POSITION_ADDR = 0xFF4A; // R/W
-const word GPU_WINDOW_X_POSITION_MINUS_7_ADDR = 0xFF4B; // R/W
+const word GPU_LCD_CONTROL_ADDR = 0xFF40;
+const word GPU_LCDC_STATUS_ADDR = 0xFF41;
+const word GPU_SCROLL_Y_ADDR = 0xFF42;
+const word GPU_SCROLL_X_ADDR = 0xFF43;
+const word GPU_LCDC_Y_CORRDINATE_ADDR = 0xFF44;
+const word GPU_LY_COMPARE_ADDR = 0xFF45;
+const word GPU_DMA_TRANSFER_AND_START_ADDR = 0xFF46;
+const word GPU_BG_PALETTE_DATA_ADDR = 0xFF47;
+const word GPU_OBJECT_PALETTE_0_DATA_ADDR = 0xFF48; 
+const word GPU_OBJECT_PALETTE_1_DATA_ADDR = 0xFF49; 
+const word GPU_WINDOW_Y_POSITION_ADDR = 0xFF4A;
+const word GPU_WINDOW_X_POSITION_MINUS_7_ADDR = 0xFF4B;
 
 /* LCD CONTROL bits */
-#define LCD_DISPLAY_ENABLE_BIT              7 // 0 off, 1 on
-#define WINDOW_TILE_MAP_DISPLAY_SELECT_BIT  6
-#define WINDOW_DISPLAY_ENABLE_BIT           5 // 0 off, 1 on
-#define BG_WINDOW_TILE_DATA_SELECT_BIT      4
-#define BG_TILE_MAP_DISPLAY_SELECT_BIT      3
-#define SPRITE_SIZE_BIT                     2 // 0=8x8, 1=8x16
-#define SPRITE_DISPLAY_ENABLE_BIT           1 // 0 off, 1 on
-#define BG_WINDOW_DISPLAY_PRIORITY_BIT      0 // 0 off, 1 on
+#define LCD_CONTROL_LCD_DISPLAY_ENABLE_BIT             7 // 0 off, 1 on
+#define LCD_CONTROL_WINDOW_TILE_MAP_DISPLAY_SELECT_BIT 6
+#define LCD_CONTROL_WINDOW_DISPLAY_ENABLE_BIT          5 // 0 off, 1 on
+#define LCD_CONTROL_BG_WINDOW_TILE_DATA_SELECT_BIT     4
+#define LCD_CONTROL_BG_TILE_MAP_DISPLAY_SELECT_BIT     3
+#define LCD_CONTROL_SPRITE_SIZE_BIT                    2 // 0=8x8, 1=8x16
+#define LCD_CONTROL_SPRITE_DISPLAY_ENABLE_BIT          1 // 0 off, 1 on
+#define LCD_CONTROL_BG_WINDOW_DISPLAY_PRIORITY_BIT     0 // 0 off, 1 on
+
+/* LCDC Status bits */
+#define LCDC_STATUS_LYC_EQUALS_LY_COINCIDENCE_INTERRUPT_BIT 6 // 1 = Enable
+#define LCDC_STATUS_MODE_2_OAM_INTERRUPT_BIT                5 // 1 = Enable
+#define LCDC_STATUS_MODE_1_V_BLANK_INTERRUPT_BIT            4 // 1 = Enable
+#define LCDC_STATUS_MODE_0_H_BLANK_INTERRUPT_BIT            3 // 1 = Enable 
+#define LCDC_STATUS_LYC_LY_COINCIDENCE_FLAG_BIT             2 // 0 if LYC < LY or LYC > LY, 1 if LYC = LY
+#define LCDC_STATUS_MODE_FLAG_SECOND_BIT                    1 // 00: H_Blank mode 01: V_Blank mode
+#define LCDC_STATUS_MODE_FLAG_FIRST_BIT                     0 // 10: Searching OAM mode 11: Transfer data to LCD mode
 
 struct Pixel
 {
@@ -85,7 +94,7 @@ private:
     const uint32_t MIN_SEARCHING_OAM_MODE_CYCLES = 80; // Mode 2
     const uint32_t MIN_TRANSFER_DATA_TO_LCD_MODE_CYCLES = 172; // Mode 3
     const uint32_t MAX_VIDEO_CYCLES = MIN_H_BLANK_MODE_CYCLES + MIN_SEARCHING_OAM_MODE_CYCLES + MIN_TRANSFER_DATA_TO_LCD_MODE_CYCLES;
-    const uint32_t V_BLANK_CYCLES = 4560; // Mode 1
+    const uint32_t V_BLANK_CYCLES = MAX_VIDEO_CYCLES * 10; // Mode 1, 10 lines 144-153
 
     bool isLCDEnabled();
     void setMode(Video_Mode i_NewMode);
@@ -93,6 +102,8 @@ private:
     void handleVBlankMode();
     void handleSearchSpritesAttributesMode();
     void handleLCDTransferMode();
+    void drawScanline();
+    bool checkForLCDCInterrupt(int i_InterruptBit);
 
     Video_Mode m_Mode;
     uint32_t m_VideoCycles;
